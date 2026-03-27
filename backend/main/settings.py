1d60c6e56cbfdc87da52043a85a421c3d5f0ffce
MGLabs <mailbox@kieran.cloud>
2026-03-27 18:16:40 -0500

    Update settings.py

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "core.middleware.AfterHoursModeMiddleware",
 ]
 
 ROOT_URLCONF = "main.urls"
